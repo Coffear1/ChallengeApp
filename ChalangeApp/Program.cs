d578fd6cec5458333e217bc0d2e7be1165a2e72b
Krzysiek <krzysiek.stebel@gmail.com>
2023-01-27 19:23:15 +0100

    Dodaj pliki projektów.

---
+++ ChalangeApp/Program.cs
@@ -0,0 +1,25 @@
+﻿var name = "Kasia";
+var sex = 'F';
+var age = 26;
+
+if (sex == 'F' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" &&  age == 33)
+{
+    Console.WriteLine(" Ewa, lat 33");
+}
+else if (sex != 'F' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+
+
+
+
+
+
+
+
+
