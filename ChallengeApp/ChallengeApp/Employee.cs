4d85924fde5563236a16763e12b18dbf41bfed44
Krzysiek <krzysiek.stebel@gmail.com>
2024-03-15 18:35:22 +0100

    zadanie domowe dzien 11

--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -1,40 +1,42 @@
 ﻿
+using System.Diagnostics;
+
 namespace ChallengeApp
 {
     public class Employee
     {
-        private List<float>grades = new List<float>();
+        private List<float> grades = new List<float>();
         public Employee(string name, string surname)
         {
             this.Name = name;
-            this.Surname = surname;           
+            this.Surname = surname;
         }
-        
+
         public string Name { get; private set; }
         public string Surname { get; private set; }
 
-        public void AddGrade(float grade)            
+        public void AddGrade(float grade)
         {
-            if(grade >= 0 && grade <= 100)                              // WALIDACJA - weryfikacja czy dane są poprawne.
+            if (grade >= 0 && grade <= 100)                              // WALIDACJA - weryfikacja czy dane są poprawne.
             {                                                           // w tym wypadku musimy dopilnować aby można było dodawać oceny 
                 this.grades.Add(grade);                                 // tylko z zakresu 0-100
             }
             else
             {
                 Console.WriteLine("invalid grade value");
-            }           
+            }
         }
 
         public void AddGrade(string grade)
         {
-            if(float.TryParse(grade, out float result))                 // TryParse - sprawdza czy sparsowany parametr na pewno się zgadza 
+            if (float.TryParse(grade, out float result))                 // TryParse - sprawdza czy sparsowany parametr na pewno się zgadza 
             {                                                           // np. czy string jest "5" a nie "Adam"
                 this.AddGrade(result);
             }
             else
             {
                 Console.WriteLine("String is not float");
-            }                                      
+            }
         }
 
         public void AddGrade(int grade)
@@ -59,9 +61,8 @@ namespace ChallengeApp
             float floatValue = (float)grade;
             this.AddGrade(floatValue);
         }
-       
 
-        public Statistics GetStatistics()                           // publiczna metoda pozwalająca zwracać obiekty
+        public Statistics GetStatisticsWithForEach()
         {
             var statistics = new Statistics();
 
@@ -70,7 +71,7 @@ namespace ChallengeApp
             statistics.Max = float.MinValue;
             statistics.Min = float.MaxValue;
 
-            foreach (var grade in this.grades) 
+            foreach (var grade in this.grades)
             {
                 statistics.Max = Math.Max(statistics.Max, grade);
                 statistics.Min = Math.Min(statistics.Min, grade);
@@ -81,5 +82,76 @@ namespace ChallengeApp
 
             return statistics;
         }
+
+        public Statistics GetStatisticsWithFor()
+        {
+            var statistics = new Statistics();
+
+            // implementacja GetStatistics
+            statistics.Average = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+            var index = 0;
+
+            for (var grade = 0; grade < this.grades.Count; grade++)
+            {
+                statistics.Max = Math.Max(statistics.Max, this.grades[index]);
+                statistics.Min = Math.Min(statistics.Min, this.grades[index]);
+                statistics.Average += this.grades[index];
+                index++;
+            }
+
+            statistics.Average = statistics.Average / this.grades.Count;
+
+            return statistics;
+        }
+
+        public Statistics GetStatisticsWithDoWhile()
+        {
+            var statistics = new Statistics();
+
+            // implementacja GetStatistics
+            statistics.Average = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+            var index = 0;
+
+            do 
+            {
+                statistics.Max = Math.Max(statistics.Max, this.grades[index]);
+                statistics.Min = Math.Min(statistics.Min, this.grades[index]);
+                statistics.Average += this.grades[index];
+                index++;
+            }while(index< this.grades.Count);
+
+            statistics.Average = statistics.Average / this.grades.Count;
+
+            return statistics;
+        }
+
+        public Statistics GetStatisticsWithWhile()
+        {
+            var statistics = new Statistics();
+
+            // implementacja GetStatistics
+            statistics.Average = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+            var index = 0;
+
+            while (index < this.grades.Count) 
+            {
+                statistics.Max = Math.Max(statistics.Max, this.grades[index]);
+                statistics.Min = Math.Min(statistics.Min, this.grades[index]);
+                statistics.Average += this.grades[index];
+                index++;
+            } 
+
+            statistics.Average = statistics.Average / this.grades.Count;
+
+            return statistics;
+        }
     }
 }
+
+
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,17 +1,37 @@
 ﻿using ChallengeApp;
 
 var employee = new Employee("Krzysiek", "Stebel");
-employee.AddGrade("Krzysiek");
-employee.AddGrade("101");
-employee.AddGrade(6);
+employee.AddGrade(4);
 employee.AddGrade(5);
-employee.AddGrade(1);
+employee.AddGrade(6);
 
-var statistics = employee.GetStatistics();
-Console.WriteLine($"Average: {statistics.Average:N2}");          // N2 określa ilośc cyfr po przecinku 
-Console.WriteLine($"Min: {statistics.Min}");                        
-Console.WriteLine($"Max: {statistics.Max}");
 
+var statistics1 = employee.GetStatisticsWithForEach();
+Console.WriteLine("Oceny liczone metodą GetStatisticsWithForEach");
+Console.WriteLine($"Average: {statistics1.Average:N2}");          // N2 określa ilośc cyfr po przecinku 
+Console.WriteLine($"Min: {statistics1.Min}");                        
+Console.WriteLine($"Max: {statistics1.Max}");
+
+Console.WriteLine("__________________________________________________");
+var statistics2 = employee.GetStatisticsWithFor();
+Console.WriteLine("Oceny liczone metodą GetStatisticsWithFor");
+Console.WriteLine($"Average: {statistics2.Average:N2}");          
+Console.WriteLine($"Min: {statistics2.Min}");
+Console.WriteLine($"Max: {statistics2.Max}");
+
+Console.WriteLine("__________________________________________________");
+var statistics3 = employee.GetStatisticsWithDoWhile();
+Console.WriteLine("Oceny liczone metodą GetStatisticsWithDoWhile");
+Console.WriteLine($"Average: {statistics3.Average:N2}");
+Console.WriteLine($"Min: {statistics3.Min}");
+Console.WriteLine($"Max: {statistics3.Max}");
+
+Console.WriteLine("__________________________________________________");
+var statistics4 = employee.GetStatisticsWithWhile();
+Console.WriteLine("Oceny liczone metodą GetStatisticsWithWhile");
+Console.WriteLine($"Average: {statistics4.Average:N2}");
+Console.WriteLine($"Min: {statistics4.Min}");
+Console.WriteLine($"Max: {statistics4.Max}");
 
 
 
